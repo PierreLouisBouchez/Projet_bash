1b7fe45d64d1da5f8b2b49b85d42aef88f54a135
PierreLouisBouchez <pl.bouchez@outlook.fr>
2019-11-07 18:11:33 +0100

    first commit

--- tableur.sh
+++ tableur.sh
@@ -1,45 +1,63 @@
 #!/bin/sh
 #echo "hello world"
-
-in=""
+int="fr"
 out=""
 scin="\t"
 scout="\t"
 slin="\n"
 slout="\n"
 inverse=0
-
-
-
-
 while test $# -gt 0 ;do
-    case $1 in
-    "-in")  if test -e ./$2
+    case "$1" in
+        ("-in")  if test -e ./$2
             then
-                echo "Fichier trouvé"
-                in=$2
+                int=$2
                 shift
             else
                 echo "Le fichier $2 n'existe pas"
                 exit 0
             fi;;
-    "-out") if test -e ./$2
-            then
-                echo "Fichier trouvé"
-                out=$2
-                shift
-            else
-                echo "Le fichier $2 n'existe pas"
-                exit 0
-            fi;;
-    "-scin") echo "-scin" ;;
-    "-scout") echo "-scout" ;;
-    "-slin") echo "-slin" ;;
-    "-slout") echo hello ;;
-    "-inverse") invers=1 ;;
-    *) exit 0;;
+        ("-out") if test -e ./$2
+                then
+                    out=$2
+                    shift
+                else
+                    echo "Le fichier $2 n'existe pas"
+                    exit 0
+                fi;;
+        ("-scin")   scin=$2
+                    shift;;
+        ("-scout") scout=$2
+                    shift;;
+        ("-slin")   slin=$2
+                    shift;;
+        ("-slout")  slin=$2
+                    shift;;
+        ("-inverse") inverse=1 ;;
+        (*) exit 0;;
     esac
     shift
 done
-echo $in
-echo $out
+if [ $slin == '\n' ]
+then
+    for i in $(cat $int)
+    do
+        echo "$i"
+    done
+else
+    for i in `cut -d "$slin" -f 1 "$int"`
+    do
+        for j in `cut -d "$scin" -f 1 "$i"`
+        do
+            echo "$j"
+        done
+    done
+fi
+#echo $int
+#echo $out
+#echo $scin
+#echo $scout
+#echo $slin
+#echo $slout
+#echo $inverse
+
