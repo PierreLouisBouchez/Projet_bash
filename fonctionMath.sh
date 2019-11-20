70368429a2f3fa935128ff2f93c023343e1e2cd8
pereira anthony <anthony_pereira1@ens.univ-artois.fr>
2019-11-20 15:02:42 +0100

    fini les fonction avec (val)

--- fonctionMath.sh
+++ fonctionMath.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 function testNumber () {
 	if test $# -ne 1 ; then
@@ -10,6 +10,29 @@ function testNumber () {
 	return 0
 }
 
+function add () {
+	if test $# -ne 2 ; then
+		echo "NOMBRE != 2"
+		exit 1
+	elif `testNumber "$1"` || `testNumber "$2"`; then
+		echo " ERREUR not int"
+		exit 1
+	fi
+	ret=`echo "$1+$2"|bc -l`
+}
+
+function sub () {
+	if test $# -ne 2 ; then
+		echo "NOMBRE != 2"
+		exit 1
+	elif `testNumber "$1"` || `testNumber "$2"`; then
+		echo " ERREUR not int"
+		exit 1
+	fi
+	ret=`echo "$1-$2"|bc -l`
+}
+
+
 function mul () {
 	if test $# -ne 2 ; then
 		echo "NOMBRE != 2"
@@ -18,7 +41,7 @@ function mul () {
 		echo " ERREUR not int"
 		exit 1
 	fi
-	return `expr "$1" \* "$2"`
+	ret=`echo "$1*$2"|bc -l`
 }
 
 function div () {
@@ -29,7 +52,7 @@ function div () {
 		echo " ERREUR not int"
 		exit 1
 	fi
-	return `expr "$1" / "$2"`
+	ret=`echo "$1*$2"|bc -l`
 }
 
 function puissance () {
@@ -40,10 +63,107 @@ function puissance () {
 		echo " ERREUR not int"
 		exit 1
 	fi
-	cpt=0
-	while test $cpt -gt
-	return `expr "$1" / "$2"`
+	if test $2 -eq 0; then
+		return 1
+	fi
+	
+	local cpt=1
+	ret="$1"
+	while test $cpt -lt $2; do
+		ret=`echo "$ret*$1"|bc -l`
+		cpt=`expr "$cpt" + 1`
+	done
 }
 
-`div 20 5`
-echo $?
+function ln () {
+	if test $# -ne 1 ; then
+		echo "NOMBRE DE D'ARGUMENTS != 1"
+		exit 1
+	elif `testNumber "$1"`; then
+		echo " ERREUR not int"
+		exit 1
+	fi
+	ret=`echo "l($1)" |bc -l`
+}
+
+function exponentiel () {
+	if test $# -ne 1 ; then
+		echo "NOMBRE DE D'ARGUMENTS != 1"
+		exit 1
+	elif `testNumber "$1"`; then
+		echo " ERREUR not int"
+		exit 1
+	fi
+	ret=`echo "e($1)" |bc -l`
+}
+
+function sqrt () {
+	if test $# -ne 1 ; then
+		echo "NOMBRE DE D'ARGUMENTS != 1"
+		exit 1
+	elif `testNumber "$1"`; then
+		echo " ERREUR not int"
+		exit 1
+	fi
+	ret=`echo "sqrt($1)" |bc -l`
+}
+
+function concat () {
+		if test $# -ne 2 ; then
+			echo "NOMBRE DE D'ARGUMENTS != 2"
+		exit 1
+	fi
+	ret="$1""$2"
+}
+
+function length () {
+	if test $# -ne 1 ; then
+		echo "NOMBRE DE D'ARGUMENTS != 1"
+		exit 1
+	fi
+	ret=`expr length $1`
+}
+
+function subsitute () {
+	if test $# -ne 3 ; then
+		echo "NOMBRE DE D'ARGUMENTS != 3"
+		exit 1
+	elif `echo "$1" | grep -q "$2"`; then
+		ret=`echo ${1/$2/$3}`
+	else 
+		ret="$2 n'est pas dans $1"
+	fi
+}
+
+function size () {
+	if test $# -ne 1 ; then
+		echo "NOMBRE DE D'ARGUMENTS != 1"
+		exit 1
+	elif test -f $1; then
+		ret=`ls -lh "$1"| cut -d " " -f5`
+	else 
+		ret=" $1 n'est pas un fichier"
+	fi
+}
+
+function lines () {
+	if test $# -ne 1 ; then
+		echo "NOMBRE DE D'ARGUMENTS != 1"
+		exit 1
+	elif test -f $1; then
+		ret=`wc -l $1 | cut -d " " -f1`
+	else 
+		ret=" $1 n'est pas un fichier"
+	fi
+}
+
+function shell () {
+	if test $# -ne 1 ; then
+		echo "NOMBRE DE D'ARGUMENTS != 1"
+		exit 1
+	fi
+	ret=`$1`
+}
+
+shell "expr 256 + 2"
+echo $ret
